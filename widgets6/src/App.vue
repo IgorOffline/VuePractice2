e8d12a271194eee092ce6c31f95c9edcdbd2c379
IgorOffline <igor@igordurbek.com>
2022-03-16 16:01:58 +0100

    Accordion

---
+++ widgets6/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Accordion from "./components/Accordion.vue";
+
+export default {
+  components: { Accordion },
+  data() {
+    return {
+      items: [
+        {
+          title: "Title1",
+          content: "Content1",
+        },
+        {
+          title: "Title2",
+          content: "Content2",
+        },
+        {
+          title: "Title3",
+          content: "Content3",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Accordion :items="items" />
+  </div>
+</template>
---
+++ widgets6/src/components/Accordion.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: ["items"],
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+  methods: {
+    onTitleClick(index) {
+      this.activeIndex = index;
+    },
+    active(index) {
+      return this.activeIndex === index ? "active" : "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div className="ui styled accordion">
+    <main v-for="(item, index) in items" :key="item.title">
+      <div :class="'title ' + active(index)" @click="onTitleClick(index)">
+        <i className="dropdown icon"></i>
+        {{ item.title }}
+      </div>
+      <div :class="'content ' + active(index)">
+        <p>{{ item.content }}</p>
+      </div>
+    </main>
+  </div>
+</template>
